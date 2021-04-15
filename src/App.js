fc79d58886420d27e4027a2c3b02496827eeb598
Your Name <you@example.com>
2021-04-16 02:13:25 +0530

    added DB fetch

--- src/App.js
+++ src/App.js
@@ -1,54 +1,90 @@
 import './App.css';
 import TextField from '@material-ui/core/TextField';
-import {useState , useEffect} from "react";
-import {Button} from '@material-ui/core';
-import {db} from "./firebase_config";
+import {
+  useState,
+  useEffect
+} from "react";
+import {
+  Button
+} from '@material-ui/core';
+import {
+  db
+} from "./firebase_config";
 import firebase from "firebase";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
 
- const [todoInput, setTodoInput] = useState("");
+  useEffect(() => {
+    getTodo();
+  }, [])
 
-useEffect(() => {
- getTodo();
-}, [ ])
+  function getTodo() {
+    db.collection("todo_app").onSnapshot(function (querySnapshot) {
+      setTodos(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          todo: doc.data().todo,
+          inprogress: doc.data().inprogress,
 
-function getTodo(){
-db.
-}
+        }))
+      );
+    });
+  }
 
 
 
 
- function addTodo(){
-  console.log("your todo input  " );
-  db.collection("todo_app").add({
-    inprogress: true,
-    timeStamp:firebase.firestore.FieldValue.serverTimestamp(),
-    todo:todoInput,
-  });
-  setTodoInput("");
-}
+  function addTodo() {
+    console.log("your todo input  ");
+    db.collection("todo_app").add({
+      inprogress: true,
+      timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
+      todo: todoInput,
+    });
+    setTodoInput("");
+  }
 
   return (
-    <div className="App">
-     <h1>My Todo App</h1>
-     <TextField className="input" id="outlined-basic" label="Enter To-DO "
-     value={todoInput}
-     onChange={(e)=>{
-       setTodoInput(e.target.value);
-      //console.log("your todo input  " + e.target.value);
-      }}
-     variant="outlined" />
-     <Button variant="contained" onClick={addTodo}>
-       Default
-       </Button>
-     <div>
-     testing
-     </div>
-    </div>
+
+    <
+    div className = "App" >
+    <
+    h1 > My Todo App < /h1> <
+    TextField className = "input"
+    id = "outlined-basic"
+    label = "Enter To-DO "
+    value = {
+      todoInput
+    }
+    onChange = {
+      (e) => {
+        setTodoInput(e.target.value);
+        //console.log("your todo input  " + e.target.value);
+      }
+    }
+    variant = "outlined" / >
+    <
+    Button variant = "contained"
+    onClick = {
+      addTodo
+    } >
+    Default <
+    /Button> 
+    
+    {todos.map((todo)=>(
+      <p>{todo.todo}</p>
+
+    )
+      )}
+    
+    <
+    div >
+    testing <
+    /div> < /
+    div >
   );
 }
 
 export default App;
- 
